620f0dcab35cc6625ac37e66cb3e3b815668b19d
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-12-16 10:49:22 +0100

    mise à jour tp14 gestion des exceptions

--- tp14_gestion_des_exceptions.py
+++ tp14_gestion_des_exceptions.py
@@ -1,34 +1,43 @@
+from tp11_redefinition_eq import *
+from tp11_redefinition_eq import AdressePostale
+
+
 class PersonneException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+    pass
 
 
 class PersonneService:
-    def inserDonnees(self, nom, prenom, adresse):
-        try:
-            if not nom:
+
+    def valider(self,personne: Personne):
+
+            if not personne.nom:
                 raise PersonneException("Le nom doit être renseigné")
-            if len(nom) < 2:
+            if len(personne.nom) < 2:
                 raise PersonneException("le nom doit avoir au moins 2 caractères")
-            if not prenom:
+            if not personne.prenom:
                 raise PersonneException(" le prenom doit être renseigné")
-            if len(prenom) < 2:
+            if len(personne.prenom) < 2:
                 raise PersonneException("Le prenom doit avoir au moins 2 caractères")
-            if not adresse:
+            if not personne.adresse:
                 raise PersonneException("L'adresse doit être renseigné")
 
-        except PersonneException as e:
-            print(f"Erreur de validation : {str(e)}")
-        else:
-            print("Validation réussie.")
-        finally:
-            print("Fin de programme")
 
 
-service = PersonneService()
 
-service.inserDonnees("", "Patrice", "1 Allée Michel Piquemal, 34830 JACOU")
-service.inserDonnees("GOJON-GERBELOT", "M", "25 Rue Moulin à vent, 73000 GERBAIX")
-service.inserDonnees("BANCE", "", "Lieut dit Michel Gros, 97200 FORT DE FRANCE")
-service.inserDonnees("NIRDE", "Jean-Noêl", "")
+# Création d'instances de Personne avec erreurs et sans erreurs
+ad1 = AdressePostale(10, "rue du pont", "34500", "Ganges")
+p1 = Personne("", "Patrice", ad1)
+p2 = Personne("GOJON-GERBELOT", "M", ad1)
+p3 = Personne("BANCE", "", ad1)
+p4 = Personne("NIRDE", "Jean-Noel", "")
+
+lpersonnes = [p1, p2, p3, p4]
+
+for p in lpersonnes:
+    try:
+        PersonneService().valider(p)
+    except PersonneException as e:
+        print(f"Erreur de validation : {e}")
+    else:
+        print(f"Validation ok : {p}")
 
